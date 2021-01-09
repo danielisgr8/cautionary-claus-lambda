5ad2dabd98c12da5bb697139048236d9d926be86
danielisgr8 <schubertmdaniel@gmail.com>
2021-01-09 03:40:39 -0800

    Return delegator result

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,10 @@ const ddb = new DynamoDBClient({ region: "us-west-2" });
 const delegator = new ConfidentialClausDelegator(ddb);
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  delegator.delegate(event.path, event.httpMethod, event);
+  const result = await delegator.delegate(event.path, event.httpMethod, event);
 
   return {
     statusCode: 200,
-    body: `hello! ${JSON.stringify(event.pathParameters)}`
+    body: `${result}`
   }
 }
